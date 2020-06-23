5cc6067ea6abba44b8e32dc9c6bbe12d65d433c6
Patryk <46764643+paT1ku@users.noreply.github.com>
2020-06-24 01:48:45 +0200

    Something wrong
    Something wrong with images after klick  door ...

--- script.js
+++ script.js
@@ -1,58 +1,100 @@
 
-// door1 click img
+
 let doorImage1 = document.getElementById('door1');
-
-doorImage1.onclick = () => {
-    var botDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg";
-    doorImage1.src = openDoor1;
-
-}
-// door2 click img
 let doorImage2 = document.getElementById('door2');
+let doorImage3 = document.getElementById('door3');
 
+let botDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg";
+let beachDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg";
+let spaceDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg";
+
+let closedDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/closed_door.svg";
+
+let numClosedDoors = 3;
+
+let openDoor1;
+let openDoor2;
+let openDoor3;
+
+let startButton = document.getElementById('start');
+
+
+const playDoor = () => {
+    numClosedDoors--;
+    if (numClosedDoors === 0) {
+        gameOver('win')
+    } else {
+        
+    }
+}
+
+const isClicked = (door) => {
+    if (door.src === closeDoorPath) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+
+
+
+
+// MVP - If/Else Generator (3 possible combinations)
+const randomChoreDoorGenerator = () => {
+    choreDoor = Math.floor(Math.random() * numClosedDoors);
+    if (choreDoor === 0) {
+      openDoor1 = botDoorPath;
+      openDoor2 = beachDoorPath;
+      openDoor3 = spaceDoorPath;
+    } else if (choreDoor === 1) {
+      openDoor2 = botDoorPath;
+      openDoor1 = beachDoorPath;
+      openDoor3 = spaceDoorPath;
+    } else {
+      openDoor3 = botDoorPath;
+      openDoor1 = beachDoorPath;
+      openDoor2 = spaceDoorPath;
+    }
+  }
+
+
+
+// door1 click img
+doorImage1.onclick = () => {
+    if (!isClicked(doorImage1)) {
+        doorImage1.src = openDoor1;
+        playDoor();
+    }
+    
+}
+
+// door2 click img
 doorImage2.onclick = () => {
-    var beachDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg";
-    doorImage2.src = openDoor2;
-
+    if (!isClicked(doorImage2)) {
+        doorImage2.src = openDoor2;
+        playDoor();
+    }
 }
 
 // door3 click img
-let doorImage3 = document.getElementById('door3');
-
 doorImage3.onclick = () => {
-    var beachDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg";
-    doorImage3.src = openDoor3;
-
-}
-
-var numClosedDoors = 3;
-
-let openDoor1
-
-let openDoor2
-
-let openDoor3
-
-const randomChoreDoorGenerator() = () => {
-    choreDoor = Math.floor(Math.random() * numClosedDoors);
-    if (choreDoor === 0) {
-        openDoor1 = botDoorPath;
-        openDoor2 = beachDoorPath;
-        openDoor3 = spaceDoorPath;
-    } else if (choreDoor === 1) {
-        openDoor1 = botDoorPath;
-        openDoor2 = beachDoorPath;
-        openDoor3 = spaceDoorPath;
-    } else {
-        openDoor1 = botDoorPath;
-        openDoor2 = beachDoorPath;
-        openDoor3 = spaceDoorPath;
+    if (!isClicked(doorImage3)) {
+        doorImage3.src = openDoor3;
+        playDoor();
     }
-
 }
 
-const playDoor = () => {
-
+const gameOver = () => {
+    if (status === 'win') {
+        startButton.innerHTML = "You win! Play again?";
+    }
 }
+
+
+
+
+
+
 
 
